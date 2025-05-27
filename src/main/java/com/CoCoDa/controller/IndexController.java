5b0d7fc71c25d74c7f40d758717559664937670a
WeDeok <wsv1250@gmail.com>
2025-05-27 17:10:58 +0900

    일부 테스트 성 코드 삭제

--- src/main/java/com/CoCoDa/controller/IndexController.java
+++ src/main/java/com/CoCoDa/controller/IndexController.java
@@ -30,9 +30,7 @@ public class IndexController {
 		
 		return "analysis";
 	}
-		
 	
-	// Index Goto
 	@GetMapping("/")
 	public String home(Locale locale, Model model) {
 		
@@ -45,13 +43,10 @@ public class IndexController {
 
 	@GetMapping("middle")
 	public JSONArray division_middle(@RequestParam String sales_divison_l_cd) {
-		// Base
-			JSONArray result = null;
-			
-			System.out.println(sales_divison_l_cd);
-			
-		// Logic
-			result = service.division_middle(sales_divison_l_cd);
+		
+		JSONArray result = null;
+		
+		result = service.division_middle(sales_divison_l_cd);
 			
 		return result;
 
@@ -59,53 +54,49 @@ public class IndexController {
 	
 	@PostMapping("result")
 	public String location(@RequestBody IndexVO param, Model model) {
-		// Base
-			String target = "result_page";
+		
+		String target = "result_page";
+		
+		ArrayList<String> sales_num = new ArrayList<String>(); 	// 상권번호
+		String sigungu_cd = null; // 시군구 코드 
+		String sigungu_nm = null; // 시군구 이름
+		
+		String sales_division_nm = null;
+		String sales_nm = null;
+		
+		sales_division_nm = service.divisionNm(param.getSales_division_s_cd());
 			
-			System.out.println(param.getSales_division_s_cd());
-			System.out.println(param);
-			
-			
-			ArrayList<String> sales_num = new ArrayList<String>(); 	// 상권번호
-			String sigungu_cd = null; // 시군구 코드 
-			String sigungu_nm = null; // 시군구 이름
-			
-			String sales_division_nm = null;
-			String sales_nm = null;
-		// Logic
-			sales_division_nm = service.divisionNm(param.getSales_division_s_cd());
-			
-			HashMap<String, Object> division = service.divisionMap(param.getSales_division_s_cd());
-			for (String string : param.getSales_nm()) {
-				sales_nm = string;
-			}
-			
-			if(division != null) {
-				model.addAttribute("division", division);
-			}
-			
-			for (String string : param.getSales_num()) {
-				sales_num.add(string);
-			}
-			
-			for (String string : param.getSigungu_cd()) {
-				sigungu_cd = string;
-			}
-			
-			
-			for (String string : param.getSigungu_nm()) {
-				sigungu_nm = string;
-			}
-			
-			model.addAttribute("cx", param.getCx());
-			model.addAttribute("cy", param.getCy());
-			model.addAttribute("sales_num", sales_num);
-			model.addAttribute("sigungu_cd",sigungu_cd);
-			model.addAttribute("sigungu_nm", sigungu_nm);
-			model.addAttribute("address",param.getAddress());
-			model.addAttribute("sales_division_s_cd", param.getSales_division_s_cd());
-			model.addAttribute("division_nm", sales_division_nm);
-			model.addAttribute("sales_nm", sales_nm);
+		HashMap<String, Object> division = service.divisionMap(param.getSales_division_s_cd());
+		for (String string : param.getSales_nm()) {
+			sales_nm = string;
+		}
+		
+		if(division != null) {
+			model.addAttribute("division", division);
+		}
+		
+		for (String string : param.getSales_num()) {
+			sales_num.add(string);
+		}
+		
+		for (String string : param.getSigungu_cd()) {
+			sigungu_cd = string;
+		}
+		
+		
+		for (String string : param.getSigungu_nm()) {
+			sigungu_nm = string;
+		}
+		
+		model.addAttribute("cx", param.getCx());
+		model.addAttribute("cy", param.getCy());
+		model.addAttribute("sales_num", sales_num);
+		model.addAttribute("sigungu_cd",sigungu_cd);
+		model.addAttribute("sigungu_nm", sigungu_nm);
+		model.addAttribute("address",param.getAddress());
+		model.addAttribute("sales_division_s_cd", param.getSales_division_s_cd());
+		model.addAttribute("division_nm", sales_division_nm);
+		model.addAttribute("sales_nm", sales_nm);
 			
 		return target;
 
