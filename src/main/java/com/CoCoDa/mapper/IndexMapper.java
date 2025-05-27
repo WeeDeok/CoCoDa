cc7175e7fe7574d76fb6ad270a97fc625fc5c82b
WeDeok <wsv1250@gmail.com>
2025-05-27 16:31:56 +0900

    초기 화면 작업분 업로드

---
+++ src/main/java/com/CoCoDa/mapper/IndexMapper.java
@@ -0,0 +1,15 @@
+package com.CoCoDa.mapper;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public interface IndexMapper {
+
+	public List<Map<String, Object>> division_middle(String sales_divison_l_cd);
+
+	public HashMap<String, Object> divisionMap(String sales_division_s_cd);
+
+	public String divisionNm(String sales_division_s_cd);
+
+}
---
+++ src/main/java/com/CoCoDa/repository/IndexDao.java
@@ -0,0 +1,49 @@
+package com.CoCoDa.repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.CoCoDa.mapper.IndexMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class IndexDao {
+	
+	@Autowired
+	private IndexMapper mapper;
+
+	public List<Map<String, Object>> division_middle(String sales_divison_l_cd) {
+
+		// Base
+		List<Map<String, Object>> result = null;
+
+		// Logic
+		result = mapper.division_middle(sales_divison_l_cd);
+
+		return result;
+	}
+
+	public HashMap<String, Object> divisionMap(String sales_division_s_cd) {
+		// Base
+		HashMap<String, Object> result = null;
+
+		// Logic
+		result = mapper.divisionMap(sales_division_s_cd);
+
+		return result;
+	}
+
+	public String divisionNm(String sales_division_s_cd) {
+		// TODO Auto-generated method stub
+		// Base
+			String result = null;
+		
+			result = mapper.divisionNm(sales_division_s_cd);
+			
+		return result;
+	}
+
+}
+	
