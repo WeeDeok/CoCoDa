4603fa02a424518f30c899c7980bfd98cdabf35d
WeDeok <wsv1250@gmail.com>
2025-06-10 16:04:40 +0900

    테이블 CRUD시 대소문자 변경없이 입력받은 값을 넘기도록 수정

--- src/test/java/com/CoCoDa/CoCoDaApplicationTests.java
+++ src/test/java/com/CoCoDa/CoCoDaApplicationTests.java
@@ -85,8 +85,6 @@ class CoCoDaApplicationTests {
 
         UserEntity saved = userRepository.save(user);
 
-        userRepository.flush();
-
         //when
         UserEntity found = userRepository.findByUserIdAndUserPw(user.getUserid(), user.getUserpw());
         String userId = userMapper.searchUserid(user.getUserid());
