bb4df4941e4ff005b73b2e52270131868de1e494
WeDeok <wsv1250@gmail.com>
2025-06-19 17:09:56 +0900

    파일 위치변경

--- src/main/java/com/CoCoDa/service/Calculate.java
+++ src/main/java/com/CoCoDa/service/Calculate.java
@@ -1,4 +1,4 @@
-package com.CoCoDa.util;
+package com.CoCoDa.service;
 
 import java.util.HashMap;
 
@@ -10,25 +10,29 @@ public class Calculate {
 
         HashMap<String, Double> result = new HashMap<>();
 
+        double compare_salesScales = 0.00;
+        double compare_avgGrowth = 0.00;
+        double compare_expectSales = 0.00;
+
         response.forEach((key, value) -> {
 
             switch (key) {
 
                 case Constant.SALES_SCALES : 
 
-                    result.put(Constant.SALES_SCALES, getSalesScales(value));
+                    result.put(Constant.SALES_SCALES, getSalesScales(value, compare_salesScales));
                     
                     break;
                 
                 case Constant.AVG_GROWTH :
 
-                    result.put(Constant.AVG_GROWTH, getAvgGrowth(value));
+                    result.put(Constant.AVG_GROWTH, getAvgGrowth(value, compare_avgGrowth));
 
                     break;
 
                 case Constant.EXPECT_SALES :
 
-                    result.put(Constant.EXPECT_SALES, getExpectSales(value));
+                    result.put(Constant.EXPECT_SALES, getExpectSales(value, compare_expectSales));
 
                     break;
 
@@ -44,9 +48,10 @@ public class Calculate {
 
     }
 
-    private double getSalesScales (double param) {
+    private double getSalesScales (double param, double compare_salesScales) {
 
-        double compare_salesScales = 20.0;
+        if (Double.isNaN(compare_salesScales) || compare_salesScales == 0) compare_salesScales = 20.0;
+
         double result_salesScales = Math.floor(param * Constant.TOTAL_CALC / compare_salesScales);
 
         if(result_salesScales >= 10){
@@ -59,9 +64,10 @@ public class Calculate {
 
     }
 
-    private double getAvgGrowth (double param) {
+    private double getAvgGrowth (double param, double compare_avgGrowth) {
 
-        double compare_avgGrowth = 2.04;
+        if (Double.isNaN(compare_avgGrowth) || compare_avgGrowth == 0) compare_avgGrowth = 2.04;
+
         double result_avgGrowth = Math.floor(param * Constant.TOTAL_CALC / compare_avgGrowth);
 
         if(result_avgGrowth >= 10){
@@ -74,9 +80,10 @@ public class Calculate {
 
     }
 
-    private double getExpectSales (double param) {
+    private double getExpectSales (double param, double compare_expectSales) {
 
-        double compare_expectSales = 100000000000.00;
+        if (Double.isNaN(compare_expectSales) || compare_expectSales == 0) compare_expectSales = 100000000000.00;
+
         double result_expectSales = Math.floor(param * 5 / compare_expectSales);
 
         if(result_expectSales >= 5){
