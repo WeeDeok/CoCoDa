3ab1f11d742856e90995540a8d508cbd2dc3efb9
WeDeok <wsv1250@gmail.com>
2025-06-23 13:38:03 +0900

    폴리곤에 사용할 색 HEX 추가(DangerGrade.java)

--- src/main/java/com/CoCoDa/Enum/DangerGrade.java
+++ src/main/java/com/CoCoDa/Enum/DangerGrade.java
@@ -2,6 +2,32 @@ package com.CoCoDa.Enum;
 
 public enum DangerGrade {
     
-    HIGH, NORMAL, LOW, RECOMMAND;
+    NOTRECOMMEND(0, "#FF0000")
+    , HIGH(0.1, "#FF6E00")
+    , NORMAL(0.3, "#FFFF00")
+    , LOW(0.5, "#2AFF00")
+    , RECOMMAND(1, "#002EFF");
+
+    private final double rate;
+    private final String colorHex;
+
+    DangerGrade(double rate, String colorHex) {
+
+        this.rate = rate;
+        this.colorHex = colorHex;
+
+    }
+
+    public double getRate() {
+
+        return rate;
+
+    }
+
+    public String getColorHex() {
+
+        return colorHex;
+
+    }
 
 }
